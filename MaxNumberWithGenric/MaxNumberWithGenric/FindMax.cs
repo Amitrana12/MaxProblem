2c1b2927b3ad9092c1acbdc8910e1d961e9ff8b9
amit <[amirana14325@gmail.com]>
2021-03-22 00:51:17 +0530

    [Amit Rana]Added::max number in String

--- MaxNumberWithGenric/MaxNumberWithGenric/FindMax.cs
+++ MaxNumberWithGenric/MaxNumberWithGenric/FindMax.cs
@@ -20,8 +20,11 @@ namespace MaxNumberWithGenric
             {
                 return thirdValue;
             }
-            throw new Exception("Either two or three inputs are same");
+            throw new Exception("Either one, two or three inputs are same");
         }
+
+       
+
         public float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
@@ -36,7 +39,24 @@ namespace MaxNumberWithGenric
             {
                 return thirdValue;
             }
-            throw new Exception("Either two or three inputs are same");
+            throw new Exception("Either one, two or three inputs are same");
+        }
+
+        public string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("Either one, two or three inputs are same");
         }
 
     }
