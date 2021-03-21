b000ebcfa2e0bfa8a6a4f73bd941282cbd13e0bb
amit <[amirana14325@gmail.com]>
2021-03-22 00:39:14 +0530

    [Amit Rana]Added::max number in interger

--- MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
+++ MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
@@ -7,6 +7,8 @@ namespace MaxNumberWithGenric
         static void Main(string[] args)
         {
             Console.WriteLine("===Welcome to my Max Number problem console app===");
+            FindMax find = new FindMax();
+            Console.WriteLine(find.MaximumIntegerNumber(10, 20, 30));
         }
     }
 }
