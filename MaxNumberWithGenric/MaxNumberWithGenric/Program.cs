ab9eecafaaf7b45a044293903b7f8bdc552e560f
amit <[amirana14325@gmail.com]>
2021-03-22 00:25:18 +0530

    [Amit Rana]Added::Welcome massege

---
+++ MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxNumberWithGenric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===Welcome to my Max Number problem console app===");
+        }
+    }
+}
