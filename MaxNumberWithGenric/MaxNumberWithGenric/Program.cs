0a2865d38ebec6677b641a3638f8cbff3156f69a
amit <[amirana14325@gmail.com]>
2021-03-22 00:45:54 +0530

    [Amit Rana]Added::max number in Float

--- MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
+++ MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
@@ -9,6 +9,8 @@ namespace MaxNumberWithGenric
             Console.WriteLine("===Welcome to my Max Number problem console app===");
             FindMax find = new FindMax();
             Console.WriteLine(find.MaximumIntegerNumber(10, 20, 30));
+            Console.WriteLine("=================================");
+            Console.WriteLine(find.MaximumFloatNumber(10.5f, 20.5f, 30.5f));
         }
     }
 }
