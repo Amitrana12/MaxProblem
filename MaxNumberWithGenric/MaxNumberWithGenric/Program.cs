8c3f7219ca21c06780cb178a91db00144a3fc10b
amit <[amirana14325@gmail.com]>
2021-03-22 01:06:42 +0530

    [Amit Rana]Added::check for more then 3 number using genric class and  test case for UC3 and

--- MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
+++ MaxNumberWithGenric/MaxNumberWithGenric/Program.cs
@@ -6,13 +6,22 @@ namespace MaxNumberWithGenric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===Welcome to my Max Number problem console app===");
+            /*Console.WriteLine("===Welcome to my Max Number problem console app===");
             FindMax find = new FindMax();
             Console.WriteLine(find.MaximumIntegerNumber(10, 20, 30));
             Console.WriteLine("=================================");
             Console.WriteLine(find.MaximumFloatNumber(10.5f, 20.5f, 30.5f));
             Console.WriteLine("=================================");
-            Console.WriteLine(find.MaximumStringNumber("Apple", "Banana", "Carrot"));
+            Console.WriteLine(find.MaximumStringNumber("Apple", "Banana", "Carrot"));*/
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            FindMax<int> first = new FindMax<int>(intArray);
+            float[] floatArray = { 11.2f, 34.4f, 4.32f, 55.5f, 6.78f };
+            FindMax<float> second = new FindMax<float>(floatArray);
+            string[] stringArray = { "111", "222", "333", "55", "999" };
+            FindMax<string> third = new FindMax<string>(stringArray);
+            Console.WriteLine(first.FindMaxValue());
+            Console.WriteLine(second.FindMaxValue());
+            Console.WriteLine(third.FindMaxValue());
         }
     }
 }
---
+++ MaxNumberWithGenric/MaxNumberWithGenricTests/FindMaxTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaxNumberWithGenric;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxNumberWithGenric.Tests
+{
+    [TestClass]
+    public class FindMaxTests
+    {
+        [TestMethod]
+        public void Maximum_Value_PassedAt_First_Parameter_Should_Return_Maximum_Value()
+        {
+            string firstValue = "Carrot", secondValue = "Banana", thirdValue = "Apple";
+            string expected = "Carrot";
+            FindMax find = new FindMax();
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+        //TC1.2
+        [TestMethod]
+        public void Maximum_Value_PassedAt_Second_Parameter_Should_Return_Maximum_Value()
+        {
+            string firstValue = "Banana", secondValue = "Carrot", thirdValue = "Apple";
+            string expected = "Carrot";
+            FindMax find = new FindMax();
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+        //TC1.3
+        [TestMethod]
+        public void Maximum_Value_PassedAt_Third_Parameter_Should_Return_Maximum_Value()
+        {
+            string firstValue = "Apple", secondValue = "Banana", thirdValue = "Carrot";
+            string expected = "Carrot";
+            FindMax find = new FindMax();
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
